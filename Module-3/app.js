3ee35b41a351bb76d2e7da0a179e96c06619297f
Paulina <paulinadesign@users.noreply.github.com>
2020-09-13 22:12:39 +0300

    Completed the assignment

--- Module-3/app.js
+++ Module-3/app.js
@@ -6,48 +6,64 @@ angular.module("NarrowItDownApp", [])
 .service("MenuSearchService", MenuSearchService)
 .directive("foundItems", FoundItems);
 
+
 function FoundItems() {
     var ddo = {
-
+        restrict: "E",
+        templateUrl: "menuList.html",
+        scope: {
+            foundItems: "<",
+            onRemove: "&"
+        }
     };
-
     return ddo;
 }
 
+
 NarrowItDownController.$inject = ["MenuSearchService"];
-function NarrowItDownController (MenuSearchService) {
+function NarrowItDownController(MenuSearchService){
     var menu = this;
     menu.searchTerm = "";
 
-    menu.found = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
-    /*found.then(
-
-    );*/
-
+    menu.getMatchedMenuItems = () => {
+	  if (menu.searchTerm !== ""){
+        return MenuSearchService.getMatchedMenuItems(menu.searchTerm)
+            .then(response => {
+                menu.found = response;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+      }
+      else {
+        return menu.found = [];
+	  }
+    };
+    
+    menu.removeItem = (index) => {
+        menu.found.splice(index, 1);
+    };
 }
 
+
 MenuSearchService.$inject = ["$http"];
-function MenuSearchService($http) {
+function MenuSearchService($http){
     var service = this;
 
-    service.getMatchedMenuItems = (searchTerm) => {
+    service.getMatchedMenuItems = (searchTerm) =>{
         return $http({
-            method: "GET",
-            url: "https://davids-restaurant.herokuapp.com/menu_items.json"
-        }).then(
-            function success(response) {
-                //LOOP THROUGH ITEMS AND PICK WHAT MATCHES SEARCH
-                let foundItems = response.data;
-                
-                console.log(response.data);
-                return foundItems;
-            },
-            function error(response) {
-                console.log("Ajajai, this is an error!");
-            }
-        );
+                    method: "GET",
+                    url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+                }).then(response => {
+                    let foundItems = [];
+                    response.data.menu_items.forEach(item => {
+                        if (item.description.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            foundItems.push(item);
+                        }
+                    });
+                    return foundItems;
+                });
     };
-
 }
 
 })();

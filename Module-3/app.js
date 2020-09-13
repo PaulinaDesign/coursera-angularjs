f8cf08419707cae736b4dec8d9bd74ee116a4a03
Paulina <paulinadesign@users.noreply.github.com>
2020-09-13 11:35:22 +0300

    Changes to Module-3 task

--- Module-3/app.js
+++ Module-3/app.js
@@ -4,27 +4,40 @@
 angular.module("NarrowItDownApp", [])
 .controller("NarrowItDownController", NarrowItDownController)
 .service("MenuSearchService", MenuSearchService)
-.directive("foundItems", foundItems);
+.directive("foundItems", FoundItems);
 
+function FoundItems() {
+    var ddo = {
+
+    };
+
+    return ddo;
+}
 
 NarrowItDownController.$inject = ["MenuSearchService"];
 function NarrowItDownController (MenuSearchService) {
-    var controller = this;
-    
-    controller.found = getMatchedMenuItems(searchTerm);
-    
+    var menu = this;
+    menu.searchTerm = "";
+
+    menu.found = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    /*found.then(
+
+    );*/
 
 }
 
-function MenuSearchService() {
-    var getMatchedMenuItems = (searchTerm) => {
+MenuSearchService.$inject = ["$http"];
+function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = (searchTerm) => {
         return $http({
             method: "GET",
             url: "https://davids-restaurant.herokuapp.com/menu_items.json"
         }).then(
             function success(response) {
                 //LOOP THROUGH ITEMS AND PICK WHAT MATCHES SEARCH
-                let foundItems = [];
+                let foundItems = response.data;
                 
                 console.log(response.data);
                 return foundItems;
@@ -33,7 +46,6 @@ function MenuSearchService() {
                 console.log("Ajajai, this is an error!");
             }
         );
-        
     };
 
 }
